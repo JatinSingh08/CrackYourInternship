3ca4ddbdcfc86e27b4adda8cf03cfea23ea22f6d
JatinSingh08 <97881261+JatinSingh08@users.noreply.github.com>
2022-05-04 23:54:30 +0530

    Create 11.Find-all-the-duplicates-in-array.cpp

---
+++ 11.Find-all-the-duplicates-in-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        if(nums.empty()) return {};
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int index=abs(nums[i])-1;
+            if(nums[index] < 0){
+                ans.push_back(abs(nums[i]));
+            }
+            else nums[index]= -1 * nums[index];
+        }
+        return ans;
+    }
+};
