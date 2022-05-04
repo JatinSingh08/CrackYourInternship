fdc32b10b7e6fe35cbe540f1bc7d86d831a6ed50
JatinSingh08 <97881261+JatinSingh08@users.noreply.github.com>
2022-05-04 23:53:16 +0530

    Create 09.Subarray-sum-divbyk.cpp

---
+++ 09.Subarray-sum-divbyk.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int rem=0;
+        int ans=0;
+        int sum=0;
+        map<int,int> mp;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            rem = sum % k;
+            if(rem<0) rem+=k;
+            
+            if(mp[rem]){
+                ans+=mp[rem];
+                mp[rem]++;
+            }
+            else{
+                mp[rem]=1;
+            }
+        }
+        
+        
+        return ans;
+    }
+};
